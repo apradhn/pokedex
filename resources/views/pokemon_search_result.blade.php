0641baff119e05976f8fa7577537d265f1828c66
Abhijit Pradhan <abhijitpradhan@Vision.local>
2015-03-06 17:36:55 -0600

    minor style tweaks

--- resources/views/pokemon_search_result.blade.php
+++ resources/views/pokemon_search_result.blade.php
@@ -1,51 +1,53 @@
 <div class="row search-results">
 	<div class="panel panel-default col-sm-8 col-sm-offset-2">
 		<div class="row">
-			<div class="col-sm-12 col-lg-6">
-				<h3 class="pokemon-name">{{ $pm['name'] }}</h3>
-				<img class="sprite" src="{{ $pm['sprite_url'] }}" alt="{{ $pm['name'] }}">
-				<div class="type">{{ $pm['type_a'] }}<br>{{ $pm ['type_b'] }}</div>
-			</div>
+			<div class="col-lg-10 col-lg-offset-1">
+				<div class="col-sm-12 col-lg-6">
+					<h3 class="pokemon-name">{{ $pm['name'] }}</h3>
+					<img class="sprite" src="{{ $pm['sprite_url'] }}" alt="{{ $pm['name'] }}">
+					<div class="type">{{ $pm['type_a'] }}<br>{{ $pm ['type_b'] }}</div>
+				</div>
 
-			<div class="col-lg-6">
-				<table class="table table-condensed base-stats col-sm-10">
-					<thead class="">
-						<tr>
-							<th>Base Stats</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody class="">
-						<tr>
-							<th>HP</th>
-							<td>{{ $pm['hp'] }}</td>
-						</tr>
-						<tr>
-							<th>Attack</th>
-							<td>{{ $pm['attack'] }}</td>
-						</tr>
-						<tr>
-							<th>Defense</th>
-							<td>{{ $pm['defense'] }}</td>
-						</tr>
-						<tr>
-							<th>Sp. Atk</th>
-							<td>{{ $pm['sp_atk'] }}</td>
-						</tr>
-						<tr>
-							<th>Sp. Def</th>
-							<td>{{ $pm['sp_def'] }}</td>
-						</tr>
-						<tr>
-							<th>Speed</th>
-							<td>{{ $pm['speed'] }}</td>
-						</tr>	
-						<tr>
-							<th class="total">Total</th>
-							<td class="total">{{ $total }}</td>
-						</tr>						
-					</tbody>		
-				</table>
+				<div class="col-lg-6">
+					<table class="table table-condensed base-stats col-sm-10">
+						<thead class="">
+							<tr>
+								<th>Base Stats</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody class="">
+							<tr>
+								<th>HP</th>
+								<td>{{ $pm['hp'] }}</td>
+							</tr>
+							<tr>
+								<th>Attack</th>
+								<td>{{ $pm['attack'] }}</td>
+							</tr>
+							<tr>
+								<th>Defense</th>
+								<td>{{ $pm['defense'] }}</td>
+							</tr>
+							<tr>
+								<th>Sp. Atk</th>
+								<td>{{ $pm['sp_atk'] }}</td>
+							</tr>
+							<tr>
+								<th>Sp. Def</th>
+								<td>{{ $pm['sp_def'] }}</td>
+							</tr>
+							<tr>
+								<th>Speed</th>
+								<td>{{ $pm['speed'] }}</td>
+							</tr>	
+							<tr>
+								<th class="total">Total</th>
+								<td class="total">{{ $total }}</td>
+							</tr>						
+						</tbody>		
+					</table>
+				</div>
 			</div>
 		</div>
 
