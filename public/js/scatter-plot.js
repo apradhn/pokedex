b65e5ac53d579a59b5ab8956f38ca167c8943606
Abhijit Pradhan <abhijitpradhan@Vision.local>
2015-03-17 20:29:32 -0500

    added averages to scatter plot

--- public/js/scatter-plot.js
+++ public/js/scatter-plot.js
@@ -1,5 +1,6 @@
 
 var pokemon = [];
+var hp = [];
 var attack = [];
 var defense = [];
 var spAtk = [];
@@ -8,6 +9,9 @@ var speed = [];
 
 for(var i = 0; i < data.length; i++) {
 	pokemon[i] = data[i]["name"];
+}
+for (var i = 0; i < data.length; i++) {
+	hp[i] = data[i]["hp"];
 }
 for (var i = 0; i < data.length; i++) {
 	attack[i] = data[i]["attack"];
@@ -26,9 +30,40 @@ for (var i = 0; i < data.length; i++) {
 	speed[i] = data[i]["speed"];
 }
 
+// Calculating the average stats for a type
+var hpSum = 0;
+var atkSum = 0;
+var defSum = 0;
+var spAtkSum = 0;
+var spDefSum = 0;
+var spdSum = 0;
+
+for (var i = 0; i < data.length-1; i++) {
+	hpSum += Number(hp[i]);
+	atkSum += Number(attack[i]);
+	defSum += Number(defense[i]);
+	spAtkSum += Number(spAtk[i]);	
+	spDefSum += Number(spDef[i]);
+	spdSum += Number(speed[i]);
+}
+
+console.log(atkSum);
+
+var hpAvg = Math.round(hpSum / data.length)
+var atkAvg = Math.round(atkSum / data.length);
+var defAvg = Math.round(defSum / data.length); 
+var spAtkAvg = Math.round(spAtkSum / data.length);
+var spDefAvg = Math.round(spDefSum / data.length);
+var spdAvg = Math.round(spdSum / data.length);
+
+console.log(hpAvg);
+
+// Initialize datasets
+
 var dataset = [];
 
 for (var i = 0; i < data.length; i++) {
+	dataset.push(["HP", hp[i] ]);
 	dataset.push(["Attack", attack[i] ]);
 	dataset.push(["Defense", defense[i] ]);
 	dataset.push(["Sp. Atk", spAtk[i] ]);
@@ -36,42 +71,39 @@ for (var i = 0; i < data.length; i++) {
 	dataset.push(["Speed", speed[i] ]);
 }
 
-
-/*
-var dataset = [ 
-				["Attack", 64], ["Defense", 45], ["Sp. Atk", 50], ["Sp. Def", 50], ["Speed", 50],
-				["Attack", 	84], ["Defense", 65], ["Sp. Atk", 70], ["Sp. Def", 70], ["Speed", 70]		
-			];
-*/
 var lineData = [];
 
 for (var i = 0; i < data.length; i++) {
 	lineData[i] = [	
-					{ "x": "Attack", "y": attack[i] }, { "x": "Defense", "y": defense[i] },
-					{ "x": "Sp. Atk", "y": spAtk[i] }, { "x": "Sp. Def", "y": spDef[i] },
+					{ "x": "HP", "y": hp[i] },
+					{ "x": "Attack", "y": attack[i] }, 
+					{ "x": "Defense", "y": defense[i] },
+					{ "x": "Sp. Atk", "y": spAtk[i] }, 
+					{ "x": "Sp. Def", "y": spDef[i] },
 					{ "x": "Speed", "y": speed[i] },
 				  ]
 }
 
-;
+var avgDataSet = [
+					["HP", hpAvg],
+					["Attack", atkAvg],
+					["Defense", defAvg],
+					["Sp. Atk", spAtkAvg],
+					["Sp. Def", spDefAvg],
+					["Speed", spdAvg]
+				 ]
+console.log(avgDataSet);
 
-/*
-var lineData = [ 
-				[
-					 { "x": "Attack", "y": 64 }, { "x": "Defense", "y": 45 }, { "x": "Sp. Atk", "y": 50 },
-					 { "x": "Sp. Def", "y": 50 }, { "x": "Speed", "y": 50 } 
-				],
-				[
-					 { "x": "Attack", "y": 84 }, { "x": "Defense", "y": 65 }, { "x": "Sp. Atk", "y": 70 },
-					 { "x": "Sp. Def", "y": 70 }, { "x": "Speed", "y": 70 } 
-				]
-			   ];	
-*/
-
-var names = [
-			 "Dratini",
-			 "Dragonair"
-			];			   		   
+var avgLineData = [
+					{ "x": "HP", "y": hpAvg },
+					{ "x": "Attack", "y": atkAvg },
+					{ "x": "Defense", "y": defAvg },
+					{ "x": "Sp. Atk", "y": spAtkAvg },
+					{ "x": "Sp. Def", "y": spDefAvg },
+					{ "x": "Speed", "y": spdAvg },
+				  ]				 
+	  
+console.log(avgLineData); 		   
 // initialize variables for svg canvas
 var w = 800;
 var h = 600;
@@ -80,12 +112,12 @@ var padding = 20;
 
 // Initialize scales
 var x = d3.scale.ordinal()
-	.domain(["Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"])
-	.rangeBands([1, w - padding ])
+	.domain(["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"])
+	.rangeBands([padding, w - padding * 2 ])
 
 var y = d3.scale.linear()
 	.domain([0, 170])
-	.range([h, 0]);
+	.range([h-padding, padding]);
 
 // Initialize line
 var line = d3.svg.line()
@@ -97,7 +129,7 @@ var line = d3.svg.line()
 var xAxis = d3.svg.axis()
 	.scale(x)
 	.orient("bottom")
-	.ticks(5);
+	.ticks(6);
 
 var yAxis = d3.svg.axis()
 	.scale(y)
@@ -118,69 +150,105 @@ var tip2 = d3.tip()
 	.attr("class", "d3-tip")
 	.html(function(d, i) { return pokemon[i]; });
 
+var avgTip = d3.tip()
+	.attr("class", "d3-tip")
+	.html("Average");
+
 //Create SVG element
 var svg = d3.select("#data")
 			.append("svg")
 			.attr("width", w)
 			.attr("height", h)
-			.append("g")
-			.attr("transform", "translate(" + margin.left + ", 0)")
 			.attr("fill", "");
 
 svg.call(tip1);
 svg.call(tip2);
+svg.call(avgTip)
 
+// rectangle 
 svg.append("rect")
-	.attr("x", 0 + padding)
+	.attr("x", padding * 2)
 	.attr("y", 0)
-	.attr("width", w-padding)
-	.attr("height", h-padding)
+	.attr("width", 740)
+	.attr("height", h - padding * 2)
 	.attr("fill", "rgba(218, 223, 225, .5)");
 
 // circles
 svg.append("g")
-	.attr("transform", "translate(" + padding * 5 + "," + -padding + ")")
 	.selectAll("circle")
 	.data(dataset)
 	.enter()
 	.append("circle")
 	.attr("cx", function(d) {
-		return x(d[0]);
+		return x(d[0]) + 85;
 	})
 	.attr("cy", function(d) {
-		return y(d[1]);
+		return y(d[1]) - 20;
 	})
 	.attr("r", 10)
-	.attr("fill", "rgba(244, 67, 54, .2)")
+	.attr("fill", "rgba(244, 67, 54, 1)")
 	.attr("stroke", "none")
+	.attr("class", "type")
 	.on("mouseover", tip1.show)
 	.on("mouseout", tip1.hide);
 
-// line
-
+// lines
 svg.append("g")
-	.attr("transform", "translate(" + padding * 5 + "," + -padding + ")")
+	.attr("transform", "translate(" + 85 + "," + -padding + ")")
 	.selectAll("path")
 	.data(lineData)
 	.enter()
 	.append("path")
 	.attr("d", function(d) { return line(d); })	
 	.attr("stroke-width", 2)
-	.attr("stroke", "rgba(0, 0, 0, .2)")
+	.attr("stroke", "rgba(244, 67, 54, 1)")
 	.attr("fill", "none")
+	.attr("class", "type")
 	.on("mouseover", tip2.show)
 	.on("mouseout", tip2.hide);		
 
+// average circles
+svg.append("g")
+	.selectAll("circle")
+	.data(avgDataSet)
+	.enter()
+	.append("circle")
+	.attr("cx", function(d) {
+		return x(d[0]) + 85;
+	})
+	.attr("cy", function(d) {
+		return y(d[1]) - 20;
+	})
+	.attr("r", 10)
+	.attr("fill", "rgb(0,150,136)")
+	.attr("class", "average")
+	.attr("stroke", "none")
+	.on("mouseover", tip1.show)
+	.on("mouseout", tip1.hide);
+
+// average lines
+svg.append("g")
+	.attr("transform", "translate(" + 85 + "," + -padding + ")")
+	.append("path")
+	.attr("d", line(avgLineData))
+	.attr("class", "average")	
+	.attr("stroke-width", 2)
+	.attr("stroke", "rgb(0,150,136)")
+	.attr("fill", "none")
+	.on("mouseover", avgTip.show)
+	.on("mouseout", avgTip.hide);		
+	
 
 // x axis
 svg.append("g")
 	.attr("class", "axis")
-	.attr("transform", "translate(" + padding + ", " + (h - padding) + ")")
+	.attr("transform", "translate(" + padding + ", " + (h - padding * 2) + ")")
 	.call(xAxis);
 
 // y axis
 svg.append("g")
 	.attr("class", "axis")
-	.attr("transform", "translate(" + 21 + "," + -padding + ")")
+	.attr("transform", "translate(" + padding*2 + "," + -padding + ")")
 	.call(yAxis);
+
 
